6a116844196c77bc7578d1bd3435ac1f430e9ed9
stollerton <stollerton@gmail.com>
2019-01-11 23:32:04 -0500

    Added speckles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from "react";
 import "./App.scss";
 import SolarSystem from "./Components/SolarSystem/SolarSystem";
+import StarFieldIMG from "./Images/Backgrounds/starfield.svg";
 
 class App extends Component {
     render() {
+        let starFieldStyle = {
+            backgroundImage: `url(${StarFieldIMG})`
+        };
         return (
             <div className="App">
                 <SolarSystem />
+                <div class="starfield" id="starset1" style={starFieldStyle} />
             </div>
         );
     }

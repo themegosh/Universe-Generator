ea6f16f4b4ac0bfa0f6fcac836e7a9a1bdc589a1
stollerton <stollerton@gmail.com>
2019-01-11 20:33:05 -0500

    New planets + glows

--- src/Components/Planet/Planet.js
+++ src/Components/Planet/Planet.js
@@ -7,7 +7,8 @@ class Planet extends Component {
         let planet = this.props.planet;
         var planetImgStyle = {
           transform: `rotate(${planet.rotation}deg)`,
-          width: `${planet.diameter * 0.3 }em`
+          width: `${planet.diameter * 0.3 }em`,
+          WebkitFilter: `drop-shadow(0px 0px 10px ${planet.atmoColor})`
         };
         return (
             <div className="planet">
